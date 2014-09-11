91756bf96e1aa27e8d54746f885ace81de488470
Talles L <talleslasmar@gmail.com>
2014-09-11 01:50:39 -0300

    little example fix

--- examples/encoding_decoding_several_numbers.js
+++ examples/encoding_decoding_several_numbers.js
@@ -9,7 +9,7 @@ var hashids = new Hashids("this is my salt");
 var id = hashids.encode(1337, 5, 77, 12345678);
 
 /* decoding that hash */
-var numbers = hashids.decode(hash);
+var numbers = hashids.decode(id);
 
 /* numbers is always an array */
 console.log(id, numbers);
